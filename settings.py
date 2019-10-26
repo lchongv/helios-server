a70895c834ca0ec803477c5a07a04b6c25e988bc
Warwick McNaughton <warwick.mcnaughton@gmail.com>
2019-10-26 17:25:19 +1300

    201910261725

--- settings.py
+++ settings.py
@@ -423,11 +423,11 @@ if ROLLBAR_ACCESS_TOKEN:
 # HEROKU                   #
 ##############################
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
